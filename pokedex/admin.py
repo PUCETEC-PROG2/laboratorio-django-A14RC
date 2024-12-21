01b62f083ac05ef5740336a83816f40c70f9400d
Andres Romero Castillo <arromeroc@puce.edu.ec>
2024-12-21 09:20:21 -0500

    Laboratorio 8 Final

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 # Register your models here.
 @admin.register(Pokemon) #el @ es un decorador, se agreag el pokemon en este caso 
 
 class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+
+
+@admin.register(Trainer) #el @ es un decorador, se agreag el pokemon en este caso 
+
+class TrainerAdmin(admin.ModelAdmin):
     pass
